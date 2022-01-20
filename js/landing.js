112999b370f090c231597eb3ddb2c1f8bf8620ad
zaher-aa <zaherabuamro@gmail.com>
2022-01-20 16:01:01 +0200

    Edited href In Landing Page Relates #13

--- js/landing.js
+++ js/landing.js
@@ -12,8 +12,7 @@ form.onsubmit = (e) => {
   localStorage.username = username;
   localStorage.category = category;
   localStorage.questionsNum = questionsNum;
-  // TODO: Change path when deploying to GitHub Pages
-  location.href = "../html/home.html";
+  location.href = "/Quizzy/html/home.html";
 };
 
 questionsLimit.oninput = (e) => {
